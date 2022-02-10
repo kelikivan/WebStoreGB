5ca36b20b5057f168a3072dca3e5924f83f284ae
ivan.kelik <ivan.kelik@rokolabs.com>
2022-02-11 02:30:58 +0500

    Added employees view

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -1,9 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Models;
 
 namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
+        private static readonly List<Employee> _employees = new()
+        {
+            new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович"},
+            new Employee { Id = 2, LastName = "Петров", FirstName = "Пётр", Patronymic = "Петрович" },
+            new Employee { Id = 3, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович" }
+        };
+
         public IActionResult Index()
         {
             //return Content("Данные из первого контроллера!");
@@ -14,6 +22,11 @@ namespace WebStore.Controllers
         public ActionResult<string> ConfiguredAction(string id, string value)
         {
             return $"Hello world! {id} - {value}";
+        }
+
+        public IActionResult Employees()
+        {
+            return View(_employees);
         }
     }
 }
