5c3ddbae72e13c42ffe73d8572b3be96dccebd57
ivan.kelik <ivan.kelik@rokolabs.com>
2022-02-14 13:37:53 +0500

    Added products controller and views

---
+++ WebStore/Controllers/ProductsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Product()
+        {
+            return View();
+        }
+    }
+}
