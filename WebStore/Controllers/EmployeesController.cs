8a3a54a51205b57e2a383cd87a8868e299afcd5a
ivan.kelik <ivan.kelik@rokolabs.com>
2022-02-23 01:48:40 +0500

    Added base interfaces for domain

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -5,140 +5,140 @@ using WebStore.ViewModels;
 
 namespace WebStore.Controllers
 {
-    //[Route("empl/[action]/{id?}")]
-    //[Route("Staff/{action=Index}/{id?}")]
-    public class EmployeesController : Controller
-    {
-        private readonly IEmployeesService _employeesService;
-        private readonly ILogger<EmployeesController> _logger;
+	//[Route("empl/[action]/{id?}")]
+	//[Route("Staff/{action=Index}/{id?}")]
+	public class EmployeesController : Controller
+	{
+		private readonly IEmployeesService _employeesService;
+		private readonly ILogger<EmployeesController> _logger;
 
-        public EmployeesController(IEmployeesService employeesService, ILogger<EmployeesController> logger)
-        {
-            _employeesService = employeesService;
-            _logger = logger;
-        }
+		public EmployeesController(IEmployeesService employeesService, ILogger<EmployeesController> logger)
+		{
+			_employeesService = employeesService;
+			_logger = logger;
+		}
 
-        public IActionResult Index()
-        {
-            var result = _employeesService.GetAll();
-            return View(result);
-        }
+		public IActionResult Index()
+		{
+			var result = _employeesService.GetAll();
+			return View(result);
+		}
 
-        //[Route("~/employees/info-{id}")]
-        public IActionResult Details(int id)
-        {
-            var employee = _employeesService.GetById(id);
-            
-            if (employee is null)
-            {
-                return NotFound();
-            }
+		//[Route("~/employees/info-{id}")]
+		public IActionResult Details(int id)
+		{
+			var employee = _employeesService.GetById(id);
+			
+			if (employee is null)
+			{
+				return NotFound();
+			}
 
-            return View(employee);
-        }
+			return View(employee);
+		}
 
-        public IActionResult Create() => View("Edit", new EmployeeEditViewModel());
+		public IActionResult Create() => View("Edit", new EmployeeEditViewModel());
 
-        public IActionResult Edit(int? id)
-        {
-            if (id is null)
-            {
-                return View(new EmployeeEditViewModel());
-            }
+		public IActionResult Edit(int? id)
+		{
+			if (id is null)
+			{
+				return View(new EmployeeEditViewModel());
+			}
 
-            var employee = _employeesService.GetById((int)id);
-            if (employee is null)
-            {
-                _logger.LogWarning("Попытка редактирования отсутствующего сотрудника с Id:{0}.", id);
-                return NotFound();
-            }
+			var employee = _employeesService.GetById((int)id);
+			if (employee is null)
+			{
+				_logger.LogWarning("Попытка редактирования отсутствующего сотрудника с Id:{0}.", id);
+				return NotFound();
+			}
 
-            var model = new EmployeeEditViewModel
-            {
-                Id = employee.Id,
-                LastName = employee.LastName,
-                FirstName = employee.FirstName,
-                Patronymic = employee.Patronymic,
-                BirthDay = employee.BirthDay,
-            };
+			var model = new EmployeeEditViewModel
+			{
+				Id = employee.Id,
+				LastName = employee.LastName,
+				FirstName = employee.FirstName,
+				Patronymic = employee.Patronymic,
+				BirthDay = employee.BirthDay,
+			};
 
-            return View(model);
-        }
-        
-        [HttpPost]
-        public IActionResult Edit(EmployeeEditViewModel model)
-        {
-            if (model.LastName.Contains("Kelik"))
-            {
-                ModelState.AddModelError("", "Себя добавлять нельзя");
-            }
+			return View(model);
+		}
+		
+		[HttpPost]
+		public IActionResult Edit(EmployeeEditViewModel model)
+		{
+			if (model.LastName.Contains("Kelik"))
+			{
+				ModelState.AddModelError("", "Себя добавлять нельзя");
+			}
 
-            if (!ModelState.IsValid)
-            {
-                return View(model);
-            }
+			if (!ModelState.IsValid)
+			{
+				return View(model);
+			}
 
-            var employee = new Employee
-            {
-                Id = model.Id,
-                LastName = model.LastName,
-                FirstName = model.FirstName,
-                Patronymic = model.Patronymic,
-                BirthDay = model.BirthDay,
-            };
+			var employee = new Employee
+			{
+				Id = model.Id,
+				LastName = model.LastName,
+				FirstName = model.FirstName,
+				Patronymic = model.Patronymic,
+				BirthDay = model.BirthDay,
+			};
 
-            if (model.Id == 0)
-            {
-                _employeesService.Add(employee);
-                _logger.LogInformation("Создан новый сотрудник {0}.", employee);
-            }
-            else if (!_employeesService.Edit(employee))
-            {
-                return NotFound();
-            }
-            else
-            {
-                _logger.LogInformation("Информация о сотруднике {0} изменена.", employee);
-            }
+			if (model.Id == 0)
+			{
+				_employeesService.Add(employee);
+				_logger.LogInformation("Создан новый сотрудник {0}.", employee);
+			}
+			else if (!_employeesService.Edit(employee))
+			{
+				return NotFound();
+			}
+			else
+			{
+				_logger.LogInformation("Информация о сотруднике {0} изменена.", employee);
+			}
 
-            return RedirectToAction("Index");
-        }
+			return RedirectToAction("Index");
+		}
 
-        public IActionResult Delete(int id)
-        {
-            if (id < 0)
-            {
-                return BadRequest();
-            }
+		public IActionResult Delete(int id)
+		{
+			if (id < 0)
+			{
+				return BadRequest();
+			}
 
-            var employee = _employeesService.GetById(id);
-            if (employee is null)
-            {
-                return NotFound();
-            }
+			var employee = _employeesService.GetById(id);
+			if (employee is null)
+			{
+				return NotFound();
+			}
 
-            var model = new EmployeeEditViewModel
-            {
-                Id = employee.Id,
-                LastName = employee.LastName,
-                FirstName = employee.FirstName,
-                Patronymic = employee.Patronymic,
-                BirthDay = employee.BirthDay,
-            };
+			var model = new EmployeeEditViewModel
+			{
+				Id = employee.Id,
+				LastName = employee.LastName,
+				FirstName = employee.FirstName,
+				Patronymic = employee.Patronymic,
+				BirthDay = employee.BirthDay,
+			};
 
-            return View(model);
-        }
+			return View(model);
+		}
 
-        [HttpPost]
-        public IActionResult DeleteConfirmed(int id)
-        {
-            if (!_employeesService.Delete(id))
-            {
-                return NotFound();
-            }
+		[HttpPost]
+		public IActionResult DeleteConfirmed(int id)
+		{
+			if (!_employeesService.Delete(id))
+			{
+				return NotFound();
+			}
 
-            _logger.LogInformation("Сотрудник с Id:{0} удалён.", id);
-            return RedirectToAction("Index");
-        }
-    }
+			_logger.LogInformation("Сотрудник с Id:{0} удалён.", id);
+			return RedirectToAction("Index");
+		}
+	}
 }
