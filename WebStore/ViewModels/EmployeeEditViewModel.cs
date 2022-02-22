5e412ad1acdc51ecd2c636704bbd8b9b7db97362
ivan.kelik <ivan.kelik@rokolabs.com>
2022-02-23 01:08:06 +0500

    Added display attributies

--- WebStore/ViewModels/EmployeeEditViewModel.cs
+++ WebStore/ViewModels/EmployeeEditViewModel.cs
@@ -1,11 +1,23 @@
-﻿namespace WebStore.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.ViewModels
 {
     public class EmployeeEditViewModel
     {
+        [HiddenInput(DisplayValue = false)]
         public int Id { get; set; }
+
+        [Display(Name = "Фамилия")]
         public string LastName { get; set; }
+
+        [Display(Name = "Имя")]
         public string FirstName { get; set; }
+
+        [Display(Name = "Отчество")]
         public string Patronymic { get; set; }
+
+        [Display(Name = "Дата рождения")]
         public DateTime BirthDay { get; set; }
     }
 }
